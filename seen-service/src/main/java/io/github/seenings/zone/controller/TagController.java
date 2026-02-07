f77709b050089922a5ba9d66ce12d954dc7c1b88
chixuehui <chixuehui@hotmail.com>
2026-02-07 23:57:38 +0800

    修复推荐使用HttpExchange #52

--- seen-service/src/main/java/io/github/seenings/zone/controller/TagController.java
+++ seen-service/src/main/java/io/github/seenings/zone/controller/TagController.java
@@ -4,43 +4,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import io.github.seenings.common.model.R;
-import io.github.seenings.common.util.ResUtils;
 import io.github.seenings.sys.constant.PublicConstant;
 import io.github.seenings.zone.model.UserTag;
-import io.github.seenings.zone.service.ITagService;
-import io.github.seenings.zone.service.ResultRecommendService;
 
-import jakarta.annotation.Resource;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
-@RestController
-@RequestMapping(PublicConstant.REST + "zone/tag")
-public class TagController {
+/// 标签
+@HttpExchange(PublicConstant.REST + "zone/tag")
+public interface TagController {
 
-    @Resource
-    private ITagService tagService;
+    @PostExchange("user-id-to-recommend-user-id")
+    R<List<Long>> userIdToRecommendUserId(@SessionAttribute Long userId);
 
-    @Resource
-    private ResultRecommendService resultRecommendService;
-
-    @PostMapping("user-id-to-recommend-user-id")
-    public R<List<Long>> userIdToRecommendUserId(@SessionAttribute Long userId) {
-        List<Long> recommendUserIds = resultRecommendService.userIdToRecommendUserId(userId);
-        return ResUtils.ok(recommendUserIds);
-    }
-
-    @PostMapping("tag")
-    public R<Map<Long, UserTag>> hotTag(@RequestBody Set<Long> userIds) {
-        Map<Long, UserTag> userIdToUserTag = tagService.userIdToUserTag(userIds);
-        return ResUtils.ok(userIdToUserTag);
-    }
+    @PostExchange("tag")
+    R<Map<Long, UserTag>> hotTag(@RequestBody Set<Long> userIds);
 
     /**
      * 根据父标签名获取标签名
@@ -48,10 +31,7 @@ public class TagController {
      * @param tagParentNames 父标签名
      * @return 父标签名对应标签名
      */
-    @PostMapping("tag-parent-name-to-tag-name")
-    public R<Map<String, String>> tagParentNameToTagName(
-            @RequestBody Set<String> tagParentNames, @RequestParam Long otherUserId) {
-        Map<String, String> tagParentNameToTagName = tagService.tagParentNameToTagName(tagParentNames, otherUserId);
-        return ResUtils.ok(tagParentNameToTagName);
-    }
+    @PostExchange("tag-parent-name-to-tag-name")
+    R<Map<String, String>> tagParentNameToTagName(
+            @RequestBody Set<String> tagParentNames, @RequestParam Long otherUserId);
 }
